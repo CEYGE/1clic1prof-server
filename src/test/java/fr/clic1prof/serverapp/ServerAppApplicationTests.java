810cae4c8ca36682f38146d3c3e2c4a3de5251ab
Syrows <syrows.development@gmail.com>
2020-12-22 19:16:05 +0100

    Setting default configuration.

--- src/test/java/fr/clic1prof/serverapp/ServerAppApplicationTests.java
+++ src/test/java/fr/clic1prof/serverapp/ServerAppApplicationTests.java
@@ -1,11 +1,8 @@
 package fr.clic1prof.serverapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest(classes = {ServerTestConfiguration.class})
+@SpringBootTest
 class ServerAppApplicationTests {
 
 }
