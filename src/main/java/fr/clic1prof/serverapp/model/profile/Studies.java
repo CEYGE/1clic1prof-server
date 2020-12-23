5744ebac62db0bc21ae147db1e62ecb4e2030de3
Syrows <syrows.development@gmail.com>
2020-12-23 17:51:26 +0100

    Adding @NotNull annotation.

--- src/main/java/fr/clic1prof/serverapp/model/profile/Studies.java
+++ src/main/java/fr/clic1prof/serverapp/model/profile/Studies.java
@@ -3,10 +3,12 @@ package fr.clic1prof.serverapp.model.profile;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Studies {
 
+    @NotNull
     @Size(max = 32)
     @JsonProperty("studies")
     private String studies;
