c04573e2b1f0ad4b3a6b8cd57a004f373ca9b7f1
Syrows <syrows.development@gmail.com>
2020-12-27 14:34:33 +0100

    Adding school level dao tests.

---
+++ src/test/java/fr/clic1prof/serverapp/dao/StudentSchoolLevelDAOTest.java
@@ -0,0 +1,33 @@
+package fr.clic1prof.serverapp.dao;
+
+import fr.clic1prof.serverapp.dao.other.IStudentSchoolLevelDAO;
+import fr.clic1prof.serverapp.model.profile.SchoolLevel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations="classpath:application-test.properties")
+public class StudentSchoolLevelDAOTest {
+
+    @Autowired
+    private IStudentSchoolLevelDAO schoolLevelDAO;
+
+    @Test
+    public void test_schoolLevels() {
+
+        List<SchoolLevel> levels = this.schoolLevelDAO.getSchoolLevels();
+
+        Assertions.assertEquals(13, levels.size());
+        Assertions.assertEquals(levels.get(0), new SchoolLevel(1, "CP"));
+        Assertions.assertEquals(levels.get(6), new SchoolLevel(7, "5ème"));
+        Assertions.assertEquals(levels.get(11), new SchoolLevel(12, "Terminale"));
+        Assertions.assertEquals(levels.get(12), new SchoolLevel(13, "Supérieur"));
+    }
+}
