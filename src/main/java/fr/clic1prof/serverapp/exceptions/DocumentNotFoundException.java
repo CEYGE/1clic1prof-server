19190e0f4c1086243f4583f8907738b5731d5afd
Syrows <syrows.development@gmail.com>
2020-12-29 21:31:56 +0100

    Adding class.

---
+++ src/main/java/fr/clic1prof/serverapp/exceptions/DocumentNotFoundException.java
@@ -0,0 +1,12 @@
+package fr.clic1prof.serverapp.exceptions;
+
+public class DocumentNotFoundException extends RuntimeException {
+
+    public DocumentNotFoundException(String message) {
+        super(message);
+    }
+
+    public DocumentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
