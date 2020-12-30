475a156e789d7048b84084bff09d1b9f6233de28
Syrows <syrows.development@gmail.com>
2020-12-30 14:07:09 +0100

    Adding response status.

--- src/main/java/fr/clic1prof/serverapp/file/exceptions/FileStorageNotFoundException.java
+++ src/main/java/fr/clic1prof/serverapp/file/exceptions/FileStorageNotFoundException.java
@@ -1,17 +1,22 @@
 package fr.clic1prof.serverapp.file.exceptions;
 
 import fr.clic1prof.serverapp.file.model.DocumentType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class FileStorageNotFoundException extends FileStorageException {
 
     private DocumentType type;
 
     public FileStorageNotFoundException(String message, DocumentType type) {
         super(message);
+        this.type = type;
     }
 
     public FileStorageNotFoundException(String message, Throwable cause, DocumentType type) {
         super(message, cause);
+        this.type = type;
     }
 
     public DocumentType getType() {
--- src/main/java/fr/clic1prof/serverapp/file/exceptions/MediaTypeNotFoundException.java
+++ src/main/java/fr/clic1prof/serverapp/file/exceptions/MediaTypeNotFoundException.java
@@ -1,5 +1,9 @@
 package fr.clic1prof.serverapp.file.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class MediaTypeNotFoundException extends RuntimeException {
 
     public MediaTypeNotFoundException(String message) {
