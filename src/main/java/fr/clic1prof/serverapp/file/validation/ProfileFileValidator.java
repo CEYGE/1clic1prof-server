01df409a861df19f94c75a8ad0f6cc73a5cf3f99
Syrows <syrows.development@gmail.com>
2020-12-30 19:24:03 +0100

    Improving file validation.

--- src/main/java/fr/clic1prof/serverapp/file/validation/ProfileFileValidator.java
+++ src/main/java/fr/clic1prof/serverapp/file/validation/ProfileFileValidator.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component("ProfileFileValidator")
-public class ProfileFileValidator implements FileValidator {
+public class ProfileFileValidator extends DefaultFileValidator {
 
     private static final int MAX_SIZE = 2 * 1024 * 1024; // 2 MB.
 
     @Override
     public boolean validate(MultipartFile file) {
+
+        if(!super.validate(file)) return false;
+
         return !file.isEmpty() && file.getSize() < MAX_SIZE;
     }
 }
